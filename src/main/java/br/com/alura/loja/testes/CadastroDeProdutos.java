c5404ece84e0fdeff5f81bc3b96fe21e742ff595
Gabriel <gabriel.castro@castrom.net>
2026-07-15 21:25:44 -0300

    Mudamos o Enum Categoria para Classe, e colocamos a cardialidade dela na Produto.java, criação da tabela categorias através da Categoria e CategoriaDao

--- src/main/java/br/com/alura/loja/testes/CadastroDeProdutos.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProdutos.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja.testes;
 
+import br.com.alura.loja.dao.CategoriaDao;
 import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.util.JPAUtil;
@@ -12,14 +13,19 @@ public class CadastroDeProdutos {
     public static void main(String[] args) {
 
         //Criando novo produto
-        Produto celular = new Produto("iPhone 5000", "Perfeito para trabalho de mídias sociais", new BigDecimal(7000), Categoria.CELULARES);
+        Categoria celulares = new Categoria("CELULARES");
+        Produto celular = new Produto("iPhone 5000", "Perfeito para trabalho de mídias sociais", new BigDecimal(7000), celulares);
 
         EntityManager em = JPAUtil.getEntityManager();
-        ProdutoDao dao = new ProdutoDao(em);
+        ProdutoDao produtoDao = new ProdutoDao(em);
+        CategoriaDao categoriaDao = new CategoriaDao(em);
 
         /*Aqui ele está fazendo um INSERT do objeto no Banco de Dados*/
         em.getTransaction().begin();
-        dao.cadastrar(celular);
+
+        categoriaDao.cadastrar(celulares);
+        produtoDao.cadastrar(celular);
+
         em.getTransaction().commit();
         em.close();
     }
